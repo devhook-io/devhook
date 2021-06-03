638f0e22ad3b30d956583bf724d58b97787cc7ba
Sutton May <sutton.may@podium.com>
2021-06-03 09:47:04 -0600

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :devhook, DevhookWeb.Endpoint,
   url: [host: "devhook.gigalixirapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: ["//*.devhook.io"]
 
 # Do not print debug messages in production
